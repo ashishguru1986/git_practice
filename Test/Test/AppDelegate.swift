b1ba06b65d2b6d5cb921d2807afbd5e1a3fe092c
ashishguru1986 <ashish.guru1986@gmail.com>
2018-01-01 16:51:48 +0530

    Update AppDelegate.swift

--- Test/Test/AppDelegate.swift
+++ Test/Test/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         //print("This is Dev")
-        print ("Edited Dev Online")
+        print ("Edited Dev Online-1")
         
         return true
     }
