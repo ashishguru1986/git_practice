62cb5ba39f03c118cf5da4d32479c587d119952b
ashishguru1986 <ashish.guru1986@gmail.com>
2018-01-01 16:39:43 +0530

    Update AppDelegate.swift

--- Test/Test/AppDelegate.swift
+++ Test/Test/AppDelegate.swift
@@ -17,7 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        print("This is Dev")
+        //print("This is Dev")
+        print ("Edited Online")
         
         return true
     }
